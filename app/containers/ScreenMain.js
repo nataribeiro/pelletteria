c47e9390bf5436f51f0ec30af1236a9d1fa2cce6
Itaua Ferreira Machado <itauafm@gmail.com>
2017-07-15 16:52:07 -0300

    montando a lista... inicio

--- app/containers/ScreenMain.js
+++ app/containers/ScreenMain.js
@@ -2,11 +2,32 @@ import React from 'react';
 import {
   Text,
   Button,
+  ListView,
+  TouchableHighlight,
   View
 } from 'react-native';
 
-
+var homeListNews = ["primeira", "Segunda"];
 export default class ScreenMain extends React.Component {
+
+  constructor() {
+    super();
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows(homeListNews),
+    };
+  }
+  renderRow(rowData, sectionID, rowID) {
+      return (
+          <TouchableHighlight 
+          onPress={this.onPressButtom}>
+            <View>
+                <Text numberOfLines={1}>{rowData}</Text>
+                <View style={{height: 1, backgroundColor: '#dddddd'}}/>
+            </View>
+          </TouchableHighlight>
+      );
+    }
 
 static navigationOptions = {
   title: 'Pagina inicial'
@@ -16,12 +37,17 @@ onPressButtom = (event) => {
   this.props.navigation.navigate('Detail');
 }
 
+ _onPressRow(rowData) {
+    this.props.navigation.navigate('Detail');
+  }
+
   render() {
     return (
       <View>
-        <Text>
-          Tela inicial
-        </Text>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this.renderRow.bind(this)}
+          />
         <Button
         title="Ir para detalhes"
         color="red"
